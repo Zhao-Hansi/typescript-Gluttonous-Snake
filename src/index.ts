08d119267dc753e22f12df72160356353e55555e
zexin.zhao <zexin.zhao@thoughtworks.com>
2023-05-17 18:30:59 +0800

    use the class method and interface demo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
-function sum(a : number, b :number){
-    return a + b
+class animal{
+    private _name : string
+    private _age : number
+    constructor(name: string, age : number) {
+        this._name = name
+        this._age = age
+    }
+    getName(){
+        return this._name
+    }
+    setName(name: string){
+        this._name = name
+    }
+    getAge(){
+        return this._age
+    }
+    setAge(age: number){
+        this._age = age
+    }
+    Animal_Sounds(bark: string){
+        console.log(bark)
+    }
 }
 
-console.log(sum(123456, 456789));
+class Dog extends animal{
+    run: string
+    constructor(name : string, age: number, run : string) {
+        super(name, age);
+        this.run = run
+    }
+}
+
+let dog = new Dog('小黄', 3 , "he is running")
+console.log(dog.getAge());
+dog.Animal_Sounds('汪汪汪汪！')
+console.log(dog.getName());
--- types.ts
+++ types.ts
@@ -1,9 +1,36 @@
-let gender : "male" | "female"
+class animal{
+    private _name : string
+    private _age : number
+    constructor(name, age) {
+        name = this._name
+        age = this._age
+    }
+    getName(){
+        return this._name
+    }
+    setName(name: string){
+        this._name = name
+    }
+    getAge(){
+        return this._age
+    }
+    setAge(age: number){
+        this._age = age
+    }
+    Animal_Sounds(bark: string){
+        console.log(bark)
+    }
+}
 
-gender = "male"
+class Dog extends animal{
+    run: string
+    constructor(name, age, run) {
+        super(name = "name", age = "age");
+        run = this.run
+    }
+}
 
-gender = "female"
-
-let a : object
-let b : {name: string}
-b ={name: 'sunwukong'}
+let dog = new Dog('小白', 3 , "he is running")
+dog.getAge()
+dog.Animal_Sounds('汪汪汪汪！')
+dog.getName()
